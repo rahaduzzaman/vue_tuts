5280a767874dd3eaa158e0ec4d808799fd82f13c
Rahaduzzaman <rahad1@ymail.com>
2017-08-04 01:05:54 +0600

    Vue event modifier

---
+++ 05_events_modifiers/app.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        name: 'Rahad',
+        age: 25,
+        X: 0,
+        Y: 0
+    },
+    methods: {
+        add: function(inc) {
+            return this.age += inc;
+        },
+        sub: function(dec) {
+            return this.age -= dec;
+        },
+        getCoOrdinate: function(event) {
+            this.X = event.offsetX;
+            this.Y = event.offsetY;
+        },
+        click: function () {
+            alert('You clicked me!');
+        }
+    }
+});

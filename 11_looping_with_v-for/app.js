9fac05491ca4b2896659a707d9e47d024024f82f
Rahaduzzaman <rahad1@ymail.com>
2017-08-09 01:04:35 +0600

    looping with v-for

---
+++ 11_looping_with_v-for/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        fruits: ['banana','apple','orange','mango'],
+        dresses: [
+            {name:'shirt',color:'white'},
+            {name:'pants',color:'black'},
+            {name:'shocks',color:'gray'}
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});

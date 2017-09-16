3209aaacf43afaf3d98d6aee62faea7bed11273e
rahad <mdrahad.d9@gmail.com>
2017-09-17 00:56:42 +0600

    dynamic components

---
+++ 27_dynamic-components/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="">
+        <keep-alive>    
+        <component v-bind:is="component"></component>
+        </keep-alive>
+        <button v-on:click="component ='form-one'">Show from One</button>
+        <button v-on:click="component ='form-two'">Show from Two</button>
+    </div>
+</template>
+
+<script>
+
+import formHelper from './components/formHelper.vue';
+import formOne from './components/form1.vue';
+import formTwo from './components/form2.vue';
+
+export default {
+    components: {
+        'form-helper': formHelper,
+        'form-one': formOne,
+        'form-two': formTwo
+    },
+    data () {
+        return {
+           component: 'form-two'
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped lang="scss">
+ button {
+     padding: 10px; 
+     border: none;
+     background-color: #cdcdcd;
+     color: #222;
+     
+ }
+</style>
---
+++ 27_dynamic-components/src/components/form2.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h2>Please fill out the form..</h2>
+        <form class="">
+            <h4>Form Two - login</h4>
+            <hr>
+            <label for="email">Email</label>
+            <input type="email" name="" id="" placeholder="Enter email">
+            <label for="password">Password</label>
+            <input type="password" name="" id="">
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    h2 {
+        text-align: center;
+    }
+    form {
+        max-width: 500px;
+        margin: 50px auto 0;
+        position: relative;
+        input {
+            display: block;
+            padding: 10px;
+            border: 1px solid #ccc;
+            margin: 10px 0;
+        }
+    }
+</style>

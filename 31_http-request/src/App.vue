3c07a194833eec97c452d0f183134e48156bdf26
rahad <mdrahad.d9@gmail.com>
2017-09-20 02:12:40 +0600

    http request using vue resource

---
+++ 31_http-request/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <form-blog></form-blog>
+    </div>
+</template>
+
+<script>
+
+import addBlog from './components/addBlog.vue';
+
+export default {
+    components: {
+        'form-blog': addBlog
+    },
+    data () {
+        return {
+          
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>

1c8604595abd5512942a67915bec41bd06a977eb
rahad <mdrahad.d9@gmail.com>
2017-09-21 02:03:27 +0600

    Get request and creating custom Directives

--- 16_vue-cli/src/App.vue
+++ 16_vue-cli/src/App.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
-        <form-blog></form-blog>
+        <show-blog></show-blog>
     </div>
 </template>
 
 <script>
 
-import addBlog from './components/addBlog.vue';
+import showBlog from './components/showBlog.vue';
 
 export default {
     components: {
-        'form-blog': addBlog
+        'show-blog': showBlog
     },
     data () {
         return {
---
+++ 16_vue-cli/src/components/showBlog.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-theme:column="'boxed'" class="blogs-wrapper">
+        <h1>All Blogs</h1>
+        <div v-for="blog in blogs" class="single-blog">
+            <h3 v-rainbow>{{ blog.title }}</h3>
+            <p>{{ blog.body }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+ 
+    data () {
+        return {
+          blogs: []
+        }
+    },
+    methods: {
+        
+    },
+    created() {
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+            this.blogs = data.body.slice(0,8); 
+        })
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .blogs-wrapper {
+        max-width: 800px;
+        margin: 0 auto;
+        h1 {
+            margin-bottom: 50px;
+        }
+        .single-blog {
+            background: #eee;
+            padding: 20px;
+            border-radius: 2px;
+            display: block;
+            margin-bottom: 20px;
+            h3 {
+                margin-bottom: 15px;
+            }
+            p {
+                font-size: 14px;
+                line-height: 26px;
+            }
+        }
+    }
+</style>
